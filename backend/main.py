16bc6bb6adb06ab30c5596b72872aa04a02ea197
Axel Hajslund Damgaard <axel@hajslunddamgaard.dk>
2024-12-06 21:41:45 +0100

    Initial commit

---
+++ backend/main.py
@@ -0,0 +1,223 @@
+#!/usr/bin/env python3
+
+#         Python Stream Deck Library
+#      Released under the MIT license
+#
+#
+
+# Example script showing some Stream Deck + specific functions
+
+import os
+import threading
+import io
+import pulsectl
+import sqlite3
+import re
+
+from PIL import Image
+from StreamDeck.DeviceManager import DeviceManager
+from StreamDeck.Devices.StreamDeck import DialEventType, TouchscreenEventType
+
+# Folder location of image assets used by this example.
+ASSETS_PATH = os.path.join(os.path.dirname(__file__), "Assets")
+
+# image for idle state
+img = Image.new('RGB', (120, 120), color='black')
+released_icon = Image.open(os.path.join(ASSETS_PATH, 'Released.png')).resize((80, 80))
+img.paste(released_icon, (20, 20), released_icon)
+
+img_byte_arr = io.BytesIO()
+img.save(img_byte_arr, format='JPEG')
+img_released_bytes = img_byte_arr.getvalue()
+
+# image for pressed state
+img = Image.new('RGB', (120, 120), color='black')
+pressed_icon = Image.open(os.path.join(ASSETS_PATH, 'Pressed.png')).resize((80, 80))
+img.paste(pressed_icon, (20, 20), pressed_icon)
+
+img_byte_arr = io.BytesIO()
+img.save(img_byte_arr, format='JPEG')
+img_pressed_bytes = img_byte_arr.getvalue()
+
+sink = []
+sink_app_name = []
+sink_media_name = []
+selected_sink = [0, 0, 0, 0]
+volume = {0: 50, 1: 50, 2: 50, 3: 50}
+
+pulse = pulsectl.Pulse('my-client-name')
+CHANGE_VOL_AMOUNT = 5
+con = sqlite3.connect("../frontend/db")
+cur = con.cursor()
+
+
+def update_db():
+    print("update_db")
+    # Write to sink
+    # Clear DB
+    query = f"DELETE FROM sink"
+    print(query)
+    cur.execute(query)
+    for i, s in enumerate(sink):
+        # Add sinks
+        escaped_media_name = str(sink_media_name[i]).replace("'", "''")
+        query = f"INSERT INTO sink VALUES ('{sink_app_name[i]}', '{escaped_media_name}', {s})"
+        print(query)
+        cur.execute(query)
+        con.commit()
+        print(i, s)
+
+    # Get selected
+    query = f"SELECT * FROM selected"
+    print(query)
+    selected = cur.execute(query)
+    for i in selected.fetchall():
+        s = i
+        print(s)
+        # s[0] MEDIA
+        # s[1] APP
+        # s[2] INDEX
+        # s[3] DIAL
+        selected_sink[s[3]] = s[2]
+
+
+def update_sinks():
+    for s in pulse.sink_input_list():
+        sink_app_name.append(s.proplist["application.name"])
+        sink_media_name.append(s.proplist["media.name"])
+        sink.append(s.index)
+
+
+def find_sink_index(app, media):
+    id = sink_media_name.index(media)
+    if sink_app_name[id] == app:
+        return sink[id]
+
+
+def change_volume(sink, direction):
+    # pulse.volume_set(sink, CHANGE_VOL_AMOUNT * direction)
+    if direction == 1:
+        os.system(f"pactl set-sink-input-volume {sink} +{CHANGE_VOL_AMOUNT}%")
+    if direction == -1:
+        os.system(f"pactl set-sink-input-volume {sink} -{CHANGE_VOL_AMOUNT}%")
+
+
+update_sinks()
+# print(sink_app_name)
+# print(sink_media_name)
+# print(sink)
+
+update_db()
+
+
+# callback when buttons are pressed or released
+def key_change_callback(deck, key, key_state):
+    print("Key: " + str(key) + " state: " + str(key_state))
+
+    deck.set_key_image(key, img_pressed_bytes if key_state else img_released_bytes)
+
+
+# callback when dials are pressed or released
+def dial_change_callback(deck, dial, event, value):
+    if event == DialEventType.PUSH:
+        print(f"dial pushed: {dial} state: {value}")
+        if dial == 3 and value:
+            deck.reset()
+            deck.close()
+        else:
+            # build an image for the touch lcd
+            img = Image.new('RGB', (800, 100), 'black')
+            icon = Image.open(os.path.join(ASSETS_PATH, 'Exit.png')).resize((80, 80))
+            img.paste(icon, (690, 10), icon)
+
+            for k in range(0, deck.DIAL_COUNT - 1):
+                img.paste(pressed_icon if (dial == k and value) else released_icon, (30 + (k * 220), 10),
+                          pressed_icon if (dial == k and value) else released_icon)
+
+            img_byte_arr = io.BytesIO()
+            img.save(img_byte_arr, format='JPEG')
+            img_byte_arr = img_byte_arr.getvalue()
+
+            deck.set_touchscreen_image(img_byte_arr, 0, 0, 800, 100)
+    elif event == DialEventType.TURN:
+        # print(f"dial {dial} turned: {value}")
+        direction = 0
+        if dial == 0:
+            if value < 0:
+                direction = -1
+            if value > 0:
+                direction = 1
+        con2 = sqlite3.connect(r"../frontend/db")
+        cur2 = con2.cursor()
+
+        query = f"SELECT * FROM selected WHERE dial={dial}"
+        print(query)
+        res = cur2.execute(query)
+        change_volume(res.fetchone()[2], direction)
+
+
+# callback when lcd is touched
+def touchscreen_event_callback(deck, evt_type, value):
+    if evt_type == TouchscreenEventType.SHORT:
+        print("Short touch @ " + str(value['x']) + "," + str(value['y']))
+
+    elif evt_type == TouchscreenEventType.LONG:
+
+        print("Long touch @ " + str(value['x']) + "," + str(value['y']))
+
+    elif evt_type == TouchscreenEventType.DRAG:
+
+        print(
+            "Drag started @ " + str(value['x']) + "," + str(value['y']) + " ended @ " + str(value['x_out']) + "," + str(
+                value['y_out']))
+
+
+if __name__ == "__main__":
+    streamdecks = DeviceManager().enumerate()
+
+    print("Found {} Stream Deck(s).\n".format(len(streamdecks)))
+
+    for index, deck in enumerate(streamdecks):
+        # This example only works with devices that have screens.
+
+        if deck.DECK_TYPE != 'Stream Deck +':
+            print(deck.DECK_TYPE)
+            print("Sorry, this example only works with Stream Deck +")
+            continue
+
+        deck.open()
+        deck.reset()
+
+        deck.set_key_callback(key_change_callback)
+        deck.set_dial_callback(dial_change_callback)
+        deck.set_touchscreen_callback(touchscreen_event_callback)
+
+        print("Opened '{}' device (serial number: '{}')".format(deck.deck_type(), deck.get_serial_number()))
+
+        # Set initial screen brightness to 30%.
+        deck.set_brightness(100)
+
+        for key in range(0, deck.KEY_COUNT):
+            deck.set_key_image(key, img_released_bytes)
+
+        # build an image for the touch lcd
+        img = Image.new('RGB', (800, 100), 'black')
+        icon = Image.open(os.path.join(ASSETS_PATH, 'Exit.png')).resize((80, 80))
+        img.paste(icon, (690, 10), icon)
+
+        for dial in range(0, deck.DIAL_COUNT - 1):
+            img.paste(released_icon, (30 + (dial * 220), 10), released_icon)
+
+        img_bytes = io.BytesIO()
+        img.save(img_bytes, format='JPEG')
+        touchscreen_image_bytes = img_bytes.getvalue()
+
+        deck.set_touchscreen_image(touchscreen_image_bytes, 0, 0, 800, 100)
+
+        # Wait until all application threads have terminated (for this example,
+        # this is when all deck handles are closed).
+        for t in threading.enumerate():
+            try:
+                t.join()
+            except RuntimeError:
+                pass
---
+++ frontend/main.pas
@@ -0,0 +1,238 @@
+unit main;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, SQLite3Conn, SQLDB, DB, Forms, Controls, Graphics, Dialogs,
+  DBGrids, StdCtrls, ActnList, Menus, ExtCtrls;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    Button1: TButton;
+    Button2: TButton;
+    Dial0: TComboBox;
+    DataSource1: TDataSource;
+    DBConnection: TSQLite3Connection;
+    Dial1: TComboBox;
+    Dial2: TComboBox;
+    Dial3: TComboBox;
+    Memo1: TMemo;
+    Panel1: TPanel;
+    Panel2: TPanel;
+    SQLQuery1: TSQLQuery;
+    SQLTransaction1: TSQLTransaction;
+    procedure Button1Click(Sender: TObject);
+    procedure Button2Click(Sender: TObject);
+    procedure Dial0Change(Sender: TObject);
+    procedure Dial1Change(Sender: TObject);
+    procedure Dial2Change(Sender: TObject);
+    procedure Dial3Change(Sender: TObject);
+    procedure FormClose(Sender: TObject; var CloseAction: TCloseAction);
+    procedure SQLite3Connection1AfterConnect(Sender: TObject);
+  private
+
+  public
+
+  end;
+
+
+
+var
+  Form1: TForm1;
+  F: TField;
+  {
+  STATIC ARRAYS
+  app: array[0..255] of String;
+  media: array[0..255] of String;
+  app_id: array[0..255] of integer;
+  dial: array[0..3] of integer;
+  }
+  app: array of String;
+  media: array of String;
+  app_id: array of integer;
+  dial: array of integer;
+  app_media: array of String;
+
+implementation
+
+operator in(const a:string;b:Array Of String):integer;inline;
+var i:integer;
+begin
+  Result := -1;
+  for i :=Low(b) to High(b) do
+  if a = b[i] then
+    begin
+      Result := i;
+      Break;
+    end;
+end;
+
+{$R *.lfm}
+
+{ TForm1 }
+
+procedure db_stuff();
+var
+  i: Integer;
+  a: String;
+  b: String;
+  c: String;
+  j: String;
+begin
+  Form1.SQLQuery1.Close;
+  Form1.SQLQuery1.SQL.Text := 'SELECT * FROM sink';
+  Form1.DBConnection.Connected := True;
+  Form1.SQLTransaction1.Active := True;
+  Form1.SQLQuery1.Open;
+  {
+  for F in SQLQuery1.Fields do begin
+    Memo1.Append(F.AsString);
+  end;
+
+  Memo1.Append(SQLQuery1.Fields[0].AsString);
+  }
+
+  app := [];
+  media := [];
+  app_id := [];
+  app_media := [];
+
+  while not Form1.SQLQuery1.EOF do
+    begin
+    // Construct a line with all field values in the current row
+
+    a := Form1.SQLQuery1.Fields[0].AsString;
+    b := Form1.SQLQuery1.Fields[1].AsString;
+    c := Form1.SQLQuery1.Fields[2].AsString;
+    {
+    Memo1.Lines.Add(a);
+    Memo1.Lines.Add(b);
+    Memo1.Lines.Add(c);
+    Memo1.Lines.Add('');
+    }
+    insert(a, app, 255);
+    insert(b, media, 255);
+    insert(c.ToInteger, app_id, 255);
+    insert(a + ': ' + b, app_media, 255);
+
+    Form1.SQLQuery1.Next;
+
+  end;
+  Form1.SQLQuery1.Close;
+  Form1.SQLTransaction1.Active := False;
+  Form1.DBConnection.Connected := False;
+
+  for j in app do
+  begin
+    Form1.Memo1.Lines.Add(j);
+  end;
+
+  Form1.Memo1.Lines.Add('');
+
+  for j in media do
+  begin
+    Form1.Memo1.Lines.Add(j);
+  end;
+
+  Form1.Memo1.Lines.Add('');
+
+  for i in app_id do
+  begin
+    Form1.Memo1.Lines.Add(i.ToString);
+  end;
+end;
+
+procedure dial_stuff();
+var
+  s: String;
+begin
+  Form1.Dial0.Items.Clear;
+  Form1.Dial1.Items.Clear;
+  Form1.Dial2.Items.Clear;
+  Form1.Dial3.Items.Clear;
+  for s in app_media do
+  begin
+    Form1.Dial0.Items.Add(s);
+    Form1.Dial1.Items.Add(s);
+    Form1.Dial2.Items.Add(s);
+    Form1.Dial3.Items.Add(s);
+  end;
+end;
+
+procedure TForm1.SQLite3Connection1AfterConnect(Sender: TObject);
+begin
+
+end;
+
+procedure TForm1.Button1Click(Sender: TObject);
+begin
+  db_stuff();
+end;
+
+procedure TForm1.Button2Click(Sender: TObject);
+begin
+  dial_stuff();
+end;
+
+procedure dial_select(index: integer; dial: integer);
+var
+  query: String;
+begin
+  //query := 'DELETE FROM selected WHERE dial = ' + dial.ToString;
+  Form1.SQLQuery1.Close;
+  Form1.SQLQuery1.SQL.Text := 'DELETE FROM selected WHERE dial = :dial';
+  Form1.SQLQuery1.Params.ParamByName('dial').AsInteger := dial;
+  Form1.Memo1.Lines.Append(Form1.SQLQuery1.SQL.Text);
+  Form1.SQLQuery1.ExecSQL;
+  Form1.SQLTransaction1.Commit;
+
+  //query := 'INSERT INTO selected VALUES (''' + app[index] + ''', ''' + media[index] + ''', ' + app_id[index].ToString + ', ' + dial.ToString + ')';
+  Form1.SQLQuery1.Close;
+  Form1.SQLQuery1.SQL.Text := 'INSERT INTO selected VALUES (:app, :media, :id, :dial)';
+  Form1.SQLQuery1.Params.ParamByName('app').AsString := app[index];   
+  Form1.SQLQuery1.Params.ParamByName('media').AsString := media[index];
+  Form1.SQLQuery1.Params.ParamByName('id').AsInteger := app_id[index];
+  Form1.SQLQuery1.Params.ParamByName('dial').AsInteger := dial;
+  Form1.Memo1.Lines.Append(Form1.SQLQuery1.SQL.Text);
+  Form1.SQLQuery1.ExecSQL;
+  Form1.SQLTransaction1.Commit
+end;
+
+procedure TForm1.Dial0Change(Sender: TObject);
+begin
+  dial_select(Dial0.ItemIndex, 0);
+end;
+
+procedure TForm1.Dial1Change(Sender: TObject);
+begin
+  dial_select(Dial1.ItemIndex, 1);
+end;
+
+procedure TForm1.Dial2Change(Sender: TObject);
+begin
+  dial_select(Dial2.ItemIndex, 2);
+end;
+
+procedure TForm1.Dial3Change(Sender: TObject);
+begin
+  dial_select(Dial3.ItemIndex, 3);
+end;
+
+procedure TForm1.FormClose(Sender: TObject; var CloseAction: TCloseAction);
+begin
+  SQLQuery1.Close;
+  SQLTransaction1.Active := False;
+  DBConnection.Connected := False;
+
+end;
+begin
+  Sleep(5000);
+  db_stuff();
+  dial_stuff();
+end.
+
